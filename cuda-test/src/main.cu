f8a07eb17735fba65067c299c6f148b9ae212591
vittorio01 <vittoriopolci@live.com>
2025-04-25 17:47:14 +0200

    added nix-shell environment

--- cuda-test/src/main.cu
+++ cuda-test/src/main.cu
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 __global__
-void print_gpu() {
-    printf("Hello\n");
+void print_gpu(void) {
+    printf("Hello from %d %d\n",threadIdx.x,blockIdx.x);
 }
 
 int main() {
-
-    print_gpu<<<1, 1>>>();
+    printf("Starting GPU program");
+    print_gpu<<<2, 1>>>();
 
     cudaDeviceSynchronize();
     return 0;
