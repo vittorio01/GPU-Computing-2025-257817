fc275a3452ad31e11d34083e7dde3a42ceb815a3
vittorio01 <vittoriopolci@live.com>
2025-05-13 17:44:43 +0200

    added documentation for libraries

--- libraries/src/dataLib/dataLib.h
+++ libraries/src/dataLib/dataLib.h
@@ -1,3 +1,5 @@
+//Library used for defining Sparse Matrix and Vectors structures and relative functions
+
 #ifndef __DATA_LIB_H__
 #define __DATA_LIB_H__
 
@@ -18,13 +20,24 @@ extern "C" {
 #include <time.h>
 #include <cuda_runtime.h>
 
-typedef enum {COO,CSR} sparseMatrixType;
+/*
+------ Sparse Matrix structure ------
+This structure contains the definition of a Sparse Matrix:
+- type -> COO or CSR, the rype of the matrix
+- rowSize -> the rows number of the matrix
+- colSize -> the columns number of the matrix
+- notNull -> the number of not null elements
 
+- rowArray -> the pointer of an int array that contains all rows coefficients in a CSR or COO format for not null elements
+- colArray -> the pointer of an int array that contains all column coefficients for not null elements
+- nutNull  -> the pointer of a float array that contains the value of all not null elements
+
+*/
+typedef enum {COO,CSR} sparseMatrixType;
 typedef struct SparseMatrix {
     int rowSize;
     int colSize;
     int notNull;
-
     sparseMatrixType type;
 
     int* rowArray;
@@ -33,6 +46,12 @@ typedef struct SparseMatrix {
 
 } SparseMatrix;
 
+/*
+------- Vector structure -------
+The structure that contains the definition of a Vector:
+- size      -> the size of the vector
+- dataArray -> A pointer for the array of floats that defines the vector
+*/
 typedef struct Vector {
     int size;
 
@@ -40,16 +59,67 @@ typedef struct Vector {
 
 } Vector;
 
+
+
+//------ Functions ------
+
+/*matrixOpen is a function that loads data from a file to a SparseMatrix structure:
+- filePath -> the String that contains the name of the file to read 
+- matrix -> an empty SparseMatrix structure where data should be loaded
+
+  returns -> the result of the operation
+*/
 int matrixOpen(char* filePath, SparseMatrix* matrix);
+
+/*matrixDestroy is a function that unload the arrays in a SparseMatrix structure:
+- matrix -> SparseMatrix structure to clear.
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t matrixDestroy(SparseMatrix* matrix);
 
+/*matriConvertCSR is a function that converts the row array of a SparseMatrix structure from the COO to the CSR format: 
+- matrix -> SparseMatrix structure to convert.
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t matrixConvertCSR(SparseMatrix* matrix);
 
+/*vectorCreate is a function that assign values to a Vector structure and initializes it to 0:
+- vector -> the vector to initialize
+- size -> the size of the vector
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t vectorCreate(Vector* vector, int size);
+
+/*vectorCreateRandom is a function that assign values to a Vector structure and initializes it with random values:
+- vector -> the vector to initialize
+- size -> the size of the vector
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t vectorCreateRandom(Vector* vector, int size);
+
+/*vectorDestroy is a function that unload the array in a Vector structure:
+- vector -> SparseMatrix structure to clear.
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t vectorDestroy(Vector* vector);
 
+/*vectorPrefetch is a function that loads the array in the Vector structure in the GPU global memory:
+- vector -> Vector structure to load.
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t vectorPrefetch(Vector* vector,int cudaDevice);
+
+/*matrixPrefetch is a function that loads the vectors in the SparseMatrix structure in the GPU global memory:
+- matrix -> SparseMatrix structure to load.
+
+  returns -> the result of the operation (cudaError_t)
+*/
 cudaError_t matrixPrefetch(SparseMatrix* matrix,int cudaDevice);
 
 #ifdef __cplusplus
--- libraries/src/mathStats/mathStats.h
+++ libraries/src/mathStats/mathStats.h
@@ -1,3 +1,5 @@
+//Lybrary that contains the definition of statistical functions used for benchmarking the algorithms
+
 #ifndef __MATH_STATS_H__ 
 #define __MATH_STATS_H__
 
@@ -8,7 +10,21 @@ extern "C" {
 #include <math.h>
 #include <stdio.h>
 
+/*math_geometric_mean performs the geometric mean of an array of floats:
+- n -> size of the array
+- numbers -> array of floats
+
+returns -> geometric mean (double)
+*/
 double math_geometric_mean(int n, float* numbers);
+
+/*math_variance performs the variance of an array of floats:
+- n -> size of the array
+- numbers -> array of floats
+- mean -> the geometric mean of the floats
+
+returns -> geometric mean (double)
+*/
 double math_variance(int n, float* numbers,float mean);
 
 #ifdef __cplusplus
